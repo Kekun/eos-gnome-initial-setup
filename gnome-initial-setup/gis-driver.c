641613165458e53c13528a72cb524418f65939b3
Philip Chimento <philip@endlessm.com>
2019-07-19 14:01:12 -0700

    account,password: Hide password page for passwordless account
    Due to the similar mechanism for hiding the password page if there is no
    local username, it seems that the GisDriver is the place to store state
    such as this, that is common to several pages.
    
    Reset the password page whenever the password page gets hidden.
    This will be useful to prevent the password from being picked up at
    the end of the FBE process if someone decides that wants to use a
    password at first and sets one but then goes back to the "account"
    page and turns off the "Password protected" switch.
    
    (Rebase 3.38: Fix minor rebase conflicts.)
    (Rebase 41.2: Fix minor rebase conflicts.)
    (Rebase 43: Fix conflicts due to GTK 4 port.)
    
    https://phabricator.endlessm.com/T22042
    https://phabricator.endlessm.com/T25162
    
    https://phabricator.endlessm.com/T35040

--- gnome-initial-setup/gis-driver.c
+++ gnome-initial-setup/gis-driver.c
@@ -67,6 +67,7 @@ typedef enum {
   PROP_LIVE_SESSION,
   PROP_LIVE_PERSISTENCE,
   PROP_USERNAME,
+  PROP_PASSWORDLESS,
   PROP_SMALL_SCREEN,
   PROP_PARENTAL_CONTROLS_ENABLED,
   PROP_FULL_NAME,
@@ -97,6 +98,7 @@ struct _GisDriver {
   gchar *lang_id;
   gchar *username;
   gchar *full_name;  /* (owned) (nullable) */
+  gboolean passwordless;
 
   GdkTexture *avatar;  /* (owned) (nullable) */
   gboolean has_default_avatar;
@@ -515,6 +517,22 @@ gis_driver_get_has_default_avatar (GisDriver *driver)
 }
 
 void
+gis_driver_set_passwordless (GisDriver *self,
+                             gboolean   passwordless)
+{
+  gboolean should_notify = (self->passwordless != passwordless);
+  self->passwordless = passwordless;
+  if (should_notify)
+    g_object_notify (G_OBJECT (self), "passwordless");
+}
+
+gboolean
+gis_driver_get_passwordless (GisDriver *self)
+{
+  return self->passwordless;
+}
+
+void
 gis_driver_set_user_permissions (GisDriver   *driver,
                                  ActUser     *user,
                                  const gchar *password)
@@ -835,6 +853,9 @@ gis_driver_get_property (GObject      *object,
     case PROP_USERNAME:
       g_value_set_string (value, driver->username);
       break;
+    case PROP_PASSWORDLESS:
+      g_value_set_boolean (value, driver->passwordless);
+      break;
     case PROP_SMALL_SCREEN:
       g_value_set_boolean (value, driver->small_screen);
       break;
@@ -872,6 +893,9 @@ gis_driver_set_property (GObject      *object,
     case PROP_USERNAME:
       g_free (driver->username);
       driver->username = g_value_dup_string (value);
+      break;
+    case PROP_PASSWORDLESS:
+      driver->passwordless = g_value_get_boolean (value);
       break;
     case PROP_PARENTAL_CONTROLS_ENABLED:
       gis_driver_set_parental_controls_enabled (driver, g_value_get_boolean (value));
@@ -1120,6 +1144,11 @@ gis_driver_class_init (GisDriverClass *klass)
     g_param_spec_string ("username", "", "",
                          NULL,
                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  obj_props[PROP_PASSWORDLESS] =
+    g_param_spec_boolean ("passwordless", "", "Main user account is passwordless",
+                          FALSE,
+                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   obj_props[PROP_SMALL_SCREEN] =
     g_param_spec_boolean ("small-screen", "", "",
